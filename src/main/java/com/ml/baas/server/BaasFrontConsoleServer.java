69b2e5089ea9d9b50462d923bad865e721c19fa5
leo <menglong567@aliyun.com>
2020-03-11 22:12:19 +0800

    add swagger2 support

--- src/main/java/com/ml/baas/server/BaasFrontConsoleServer.java
+++ src/main/java/com/ml/baas/server/BaasFrontConsoleServer.java
@@ -2,11 +2,13 @@ package com.ml.baas.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author mengl
  */
 @SpringBootApplication(scanBasePackages = "com.ml")
+@EnableSwagger2
 public class BaasFrontConsoleServer {
     public static void main(String[] args) {
         SpringApplication.run(BaasFrontConsoleServer.class, args);
