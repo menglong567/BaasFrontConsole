67301f1b7e3ba39ba06ec224841550962b4b4b9d
leo <menglong567@aliyun.com>
2021-01-16 15:28:28 +0800

    update

--- src/main/java/com/ml/baas/controller/corda/CordaNodeController.java
+++ src/main/java/com/ml/baas/controller/corda/CordaNodeController.java
@@ -1,4 +1,8 @@
 package com.ml.baas.controller.corda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CordaNodeController {
+    private static final Logger log = LoggerFactory.getLogger(CordaNodeController.class);
 }
